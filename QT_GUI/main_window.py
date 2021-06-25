41601f56413f09c75ef917531fccc79e9411f296
MUIphysiologie <maximilian.zeidler@student.i-med.ac.at>
2021-06-25 10:53:30 +0200

    added QT_file

---
+++ QT_GUI/main_window.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main_window.ui'
+##
+## Created by: Qt User Interface Compiler version 6.1.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import *  # type: ignore
+from PySide6.QtGui import *  # type: ignore
+from PySide6.QtWidgets import *  # type: ignore
+
+from self_config_notebook_widget import Config_Widget
+from online_analysis_widget import Online_Analysis
+
+import Figures_rc
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(1288, 852)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.self_configuration = QPushButton(self.centralwidget)
+        self.self_configuration.setObjectName(u"self_configuration")
+        self.self_configuration.setGeometry(QRect(20, 270, 241, 101))
+        self.online_analysis = QPushButton(self.centralwidget)
+        self.online_analysis.setObjectName(u"online_analysis")
+        self.online_analysis.setGeometry(QRect(20, 390, 241, 101))
+        self.offline_analysis = QPushButton(self.centralwidget)
+        self.offline_analysis.setObjectName(u"offline_analysis")
+        self.offline_analysis.setGeometry(QRect(20, 510, 241, 101))
+        self.statistics = QPushButton(self.centralwidget)
+        self.statistics.setObjectName(u"statistics")
+        self.statistics.setGeometry(QRect(20, 630, 241, 101))
+        self.user_communication_frame = QFrame(self.centralwidget)
+        self.user_communication_frame.setObjectName(u"user_communication_frame")
+        self.user_communication_frame.setGeometry(QRect(320, 660, 931, 80))
+        self.user_communication_frame.setFrameShape(QFrame.StyledPanel)
+        self.user_communication_frame.setFrameShadow(QFrame.Raised)
+        self.notebook = QStackedWidget(self.centralwidget)
+        self.notebook.setObjectName(u"notebook")
+        self.notebook.setGeometry(QRect(320, 30, 931, 621))
+        self.config = Config_Widget()
+        self.config.setObjectName(u"config")
+        self.notebook.addWidget(self.config)
+        self.online = Online_Analysis()
+        self.online.setObjectName(u"online")
+        self.notebook.addWidget(self.online)
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(20, 30, 241, 221))
+        self.label.setPixmap(QPixmap(u"../Logo/New_LOGO_2.png"))
+        self.label.setScaledContents(True)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 1288, 24))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.self_configuration.setText(QCoreApplication.translate("MainWindow", u"Self Configuration", None))
+        self.online_analysis.setText(QCoreApplication.translate("MainWindow", u"Online Analysis", None))
+        self.offline_analysis.setText(QCoreApplication.translate("MainWindow", u"Offline Analysis", None))
+        self.statistics.setText(QCoreApplication.translate("MainWindow", u"Statistics", None))
+        self.label.setText("")
+
+class MainWindow(QWidget,Ui_MainWindow):
+    def __init__(self,parent = None):
+        QWidget.__init__(self,parent)
+        self.setupUi(self)
+    # retranslateUi
+
--- QT_GUI/self_config_notebook_widget.py
+++ QT_GUI/self_config_notebook_widget.py
@@ -1,39 +1,56 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'self_config_notebook_widget.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
+################################################################################
+## Form generated from reading UI file 'self_config_notebook_widget.ui'
+##
+## Created by: Qt User Interface Compiler version 6.1.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import *  # type: ignore
+from PySide6.QtGui import *  # type: ignore
+from PySide6.QtWidgets import *  # type: ignore
+
 
 class Ui_Config_Widget(object):
     def setupUi(self, Config_Widget):
-        Config_Widget.setObjectName("Config_Widget")
+        if not Config_Widget.objectName():
+            Config_Widget.setObjectName(u"Config_Widget")
         Config_Widget.resize(961, 651)
-        self.Notebook = QtWidgets.QTabWidget(Config_Widget)
-        self.Notebook.setGeometry(QtCore.QRect(20, 0, 931, 621))
-        self.Notebook.setTabShape(QtWidgets.QTabWidget.Rounded)
-        self.Notebook.setObjectName("Notebook")
-        self.experiment_initialization = QtWidgets.QWidget()
-        self.experiment_initialization.setObjectName("experiment_initialization")
+        self.Notebook = QTabWidget(Config_Widget)
+        self.Notebook.setObjectName(u"Notebook")
+        self.Notebook.setGeometry(QRect(20, 0, 931, 621))
+        self.Notebook.setTabShape(QTabWidget.Rounded)
+        self.experiment_initialization = QWidget()
+        self.experiment_initialization.setObjectName(u"experiment_initialization")
         self.Notebook.addTab(self.experiment_initialization, "")
-        self.batch_communication = QtWidgets.QWidget()
-        self.batch_communication.setObjectName("batch_communication")
+        self.batch_communication = QWidget()
+        self.batch_communication.setObjectName(u"batch_communication")
         self.Notebook.addTab(self.batch_communication, "")
-        self.camera = QtWidgets.QWidget()
-        self.camera.setObjectName("camera")
+        self.camera = QWidget()
+        self.camera.setObjectName(u"camera")
         self.Notebook.addTab(self.camera, "")
 
         self.retranslateUi(Config_Widget)
+
         self.Notebook.setCurrentIndex(0)
-        QtCore.QMetaObject.connectSlotsByName(Config_Widget)
+
+
+        QMetaObject.connectSlotsByName(Config_Widget)
+    # setupUi
 
     def retranslateUi(self, Config_Widget):
-        _translate = QtCore.QCoreApplication.translate
-        Config_Widget.setWindowTitle(_translate("Config_Widget", "Form"))
-        self.Notebook.setTabText(self.Notebook.indexOf(self.experiment_initialization), _translate("Config_Widget", "Experiment Initialization"))
-        self.Notebook.setTabText(self.Notebook.indexOf(self.batch_communication), _translate("Config_Widget", "Batch Communication"))
-        self.Notebook.setTabText(self.Notebook.indexOf(self.camera), _translate("Config_Widget", "Camera"))
+        Config_Widget.setWindowTitle(QCoreApplication.translate("Config_Widget", u"Form", None))
+        self.Notebook.setTabText(self.Notebook.indexOf(self.experiment_initialization), QCoreApplication.translate("Config_Widget", u"Experiment Initialization", None))
+        self.Notebook.setTabText(self.Notebook.indexOf(self.batch_communication), QCoreApplication.translate("Config_Widget", u"Batch Communication", None))
+        self.Notebook.setTabText(self.Notebook.indexOf(self.camera), QCoreApplication.translate("Config_Widget", u"Camera", None))
+    # retranslateUi
+
+
+class Config_Widget(QWidget,Ui_Config_Widget):
+    def __init__(self,parent = None):
+        QWidget.__init__(self,parent)
+        self.setupUi(self)
+
 
