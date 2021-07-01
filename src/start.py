72e55e9d9999e0ed9ec8a13ca4eb20566fd0720a
MUIphysiologie <maximilian.zeidler@student.i-med.ac.at>
2021-07-02 00:08:47 +0200

    self configuratin and camera is working in pyqt now

--- src/start.py
+++ src/start.py
@@ -20,15 +20,25 @@ class MainWindow(QMainWindow):
             button.setProperty('class', 'big_button')
             button.clicked.connect(partial(self.ui.notebook.setCurrentIndex, i))
 
+        self.ui.statistics_2.setProperty("class", "big_button")
 
-        print(self.ui.config.Load_meta_data_experiment_12)
+
+        #print(self.ui.config.Load_meta_data_experiment_12)
 
         # connect to the metadata file path 
-        self.ui.config.Load_meta_data_experiment_12.clicked.connect(self.ui.config.open_directory)
-        self.ui.config.pushButton.clicked.connect(self.ui.config.initialize_camera)
-        self.ui.config.button_start_camera.clicked.connect(self.ui.config.start_camera_timer)
+        self.ui.config.Load_meta_data_experiment_12.clicked.connect(self.ui.config.meta_open_directory) # initialize meta data sheet opening boehringer special
+
+        #connect to the camera control
+        self.ui.config.pushButton.clicked.connect(self.ui.config.initialize_camera) # initalize the camera 
+        self.ui.config.button_start_camera.clicked.connect(self.ui.config.start_camera_timer) # intialize 
         self.ui.config.button_stop_camera.clicked.connect(self.ui.config.stop_camera)
         self.ui.config.button_take_snapshot.clicked.connect(self.ui.config.show_snapshot)
+
+        #connect to the function for the batch communication
+        self.ui.config.button_batch_1.clicked.connect(self.ui.config.open_batch_path)
+        self.ui.config.button_control_1.clicked.connect(self.ui.config.generate_control_file)
+        self.ui.config.button_submit_command.clicked.connect(self.ui.config.get_commands_from_textinput)
+        self.ui.config.button_clear_window.clicked.connect(self.ui.config.end_communication_control)
         # initialize the camera module
 
     #def select_file(self):
