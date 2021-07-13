568bbd98e44ccde56f042587956fc6841d42e02e
David <david.zimmermann@tu-dresden.de>
2021-07-14 00:27:06 +0200

    added discard option inside the treeview - reinsertion in progress

--- src/treeview_manager.py
+++ src/treeview_manager.py
@@ -5,12 +5,13 @@ from PySide6.QtWidgets import *
 from PySide6 import *
 import re
 import heka_reader
+from functools import partial
 class TreeViewManager():
     ''' Main class to handle interactions with treeviews. In general two  usages are defined right now:
     1) read multiple .dat files from a directory and create representative treeview + write all the data into a datbase
     2) read a single .dat file '''
 
-    def create_treeview_from_directory(self, tree, database,dat_files,directory_path):
+    def create_treeview_from_directory(self, tree, discarded_tree, database,dat_files,directory_path):
         '''
         creates a treeview from multiple .dat files in a directory,
         :param tree: QTreeWidget
@@ -24,11 +25,11 @@ class TreeViewManager():
             bundle = heka_reader.Bundle(file)
             # add the experiment name into experiment table
             database.add_experiment_to_experiment_table(i)
-            tree = self.create_treeview_from_single_dat_file([], bundle, "", [],tree, i,database,1)
+            tree, discarded_tree = self.create_treeview_from_single_dat_file([], bundle, "", [],tree, discarded_tree, i,database,1)
 
-        return tree
+        return tree, discarded_tree
 
-    def create_treeview_from_single_dat_file(self, index, bundle, parent, node_list, tree, experiment_name, database,data_base_mode):
+    def create_treeview_from_single_dat_file(self, index, bundle, parent, node_list, tree, discarded_tree, experiment_name, database,data_base_mode):
         '''
 
         :param index:
@@ -68,14 +69,23 @@ class TreeViewManager():
             metadata.append(node)
 
         discard_button = QPushButton()
+
+
         pixmap = QPixmap(os.getcwd()[:-3] + "\Gui_Icons\discard_red_cross_II.png")
         discard_button.setIcon(pixmap)
+
 
         if "Pulsed" in node_type:
             print("skipped")
             parent = ""
+
         if "Group" in node_type:
-            parent = QTreeWidgetItem(tree)
+            top_level_item_amount= tree.topLevelItemCount()
+            if top_level_item_amount == 0:
+                parent = QTreeWidgetItem(tree)
+            else:
+                parent = QTreeWidgetItem(top_level_item_amount)
+
             if data_base_mode:
                 parent.setText(0, experiment_name)
                 tree.addTopLevelItem(parent)
@@ -85,9 +95,10 @@ class TreeViewManager():
                 parent.setData(3, 0, [0])  # hard coded tue to .dat file structure
 
             # add discard button in coloumn 2
+            discard_button = QPushButton()
+            discard_button.setIcon(pixmap)
+            discard_button.clicked.connect(partial(self.discard_button_clicked,parent,tree,discarded_tree))
             tree.setItemWidget(parent,2,discard_button)
-            discard_button.clicked.connect(self.discard_button_clicked)
-            #discard_button.setProperty("identifier","experimentName_seriesIdentifier")
 
 
         if "Series" in node_type:
@@ -107,11 +118,18 @@ class TreeViewManager():
             else:
                 data.append(series_number - 1)
             child.setData(3, 0, data)
-            child.setData(4,0,node_type)
-            child.setExpanded(True)
-            parent = child
-            tree.setItemWidget(child,2,discard_button)
 
+            # often the specific series identifier will be needed to ensure unique identification of series
+            # whereas the user will the series name instead
+            child.setData(4,0,node_type)
+
+            child.setExpanded(False)
+            parent = child
+
+            discard_button = QPushButton()
+            discard_button.setIcon(pixmap)
+            discard_button.clicked.connect(partial(self.discard_button_clicked,child,tree,discarded_tree))
+            tree.setItemWidget(child, 2, discard_button)
 
         if "Sweep" in node_type:
             child = QTreeWidgetItem(parent)
@@ -138,10 +156,10 @@ class TreeViewManager():
 
 
         for i in range(len(node.children)):
-            self.create_treeview_from_single_dat_file(index + [i], bundle, parent, node_list, tree, experiment_name,
-                                                      database,data_base_mode)
+            self.create_treeview_from_single_dat_file(index + [i], bundle, parent, node_list, tree, discarded_tree, experiment_name,
+                                                      database, data_base_mode)
 
-        return tree
+        return tree, discarded_tree
 
     def get_number_from_string(self,string):
         '''split something like Series1 into Series,1'''
@@ -165,8 +183,63 @@ class TreeViewManager():
             if parent.child(c).childCount()>0:
                 self.uncheck_parents_childs(parent.child(c))
 
+    def discard_button_clicked(self,item,experiment_tree,discarded_tree):
 
-    @Slot()
-    def discard_button_clicked(self):
-        print("a discard button was clicked")
+        if ".dat" in item.text(0):
+            # this will be executed for .dat files
+            # @todo needs to be eddited for group in online_analysis
+            self.move_experiment_to_discarded_treeview(item,experiment_tree,discarded_tree)
+            print(".dat discarded")
+            #database.move_experiment_to_discarded_experiments_table(item.text(0))
+        else:
+            self.move_series_to_discarded_treeview(item,experiment_tree,discarded_tree)
+            #move_series_to_discarded_series_table(item.data(4,0)) # data(4,0) = series identifier
+            print("series discarded")
+
+
+    def move_experiment_to_discarded_treeview(self,item,experiment_tree,discarded_tree):
+        '''move .dat and its specific childs '''
+
+        index_of_item_to_delete = experiment_tree.indexOfTopLevelItem(item)
+        experiment_tree.takeTopLevelItem(index_of_item_to_delete)
+        discarded_tree.addTopLevelItem(item)
+
+
+    def move_series_to_discarded_treeview(self,item,experiment_tree,discarded_tree):
+        '''discard a series and it's childs (=sweeps)'''
+        parent = item.parent()
+        parent_index = experiment_tree.indexOfTopLevelItem(parent)
+        parent_text = parent.text(0)
+        item_index = parent.indexOfChild(item)
+        discarded_tree_top_level_amount = discarded_tree.topLevelItemCount()
+        pixmap = QPixmap(os.getcwd()[:-3]+"\Gui_Icons\discard_red_cross_II.png")
+
+
+
+        # 1) remove the series item and its child from the experiment tree view
+        experiment_tree.topLevelItem(parent_index).takeChild(item_index)
+
+        # 2) check if parent is already existent in the discarded view
+        for i in range(discarded_tree_top_level_amount):
+            if parent_text == discarded_tree.topLevelItem(i).text(0):
+                print("parent is already there")
+                child_amount =discarded_tree.topLevelItem(i).childCount()
+                # insert to the last position
+                discarded_tree.topLevelItem(i).insertChild(child_amount,item)
+                reinsert_button = QPushButton()
+                #reinsert_button.setText("Click")
+                inserted_item = discarded_tree.topLevelItem(i).child(child_amount)
+                reinsert_button.setIcon(pixmap)
+                #reinsert_button.clicked.connect(partial(self.reinsert_button_button_clicked, item, experiment_tree, discarded_tree))
+                discarded_tree.setItemWidget(inserted_item, 2, reinsert_button)
+                return
+
+        # 3) add a new topLevelItem if no one was found before
+        new_parent = QTreeWidgetItem(discarded_tree_top_level_amount)
+        new_parent.setText(0,parent_text)
+        new_parent.setFlags(new_parent.flags() | Qt.ItemIsUserCheckable)
+        new_parent.setCheckState(1, Qt.Unchecked)
+        discarded_tree.addTopLevelItem(new_parent)
+        discarded_tree.topLevelItem(discarded_tree_top_level_amount).insertChild(item_index, item)
+
 
