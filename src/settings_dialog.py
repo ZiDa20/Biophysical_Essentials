15eca1ba861ae4e6d2875cc5763d16661c0aba67
mouzkolit <mouzkolit@outlook.de>
2021-07-29 17:05:52 +0200

    added new layouts to self configuration, added better dark-lightmode, added konsole gui

---
+++ src/settings_dialog.py
@@ -0,0 +1,52 @@
+import sys
+import os
+sys.path.append(os.getcwd()[:-3] + "QT_GUI")
+import pandas as pd
+from settings_window import * 
+from start import *
+from qt_material import apply_stylesheet
+from tkinter_camera import * 
+
+
+class SettingsWindow(QWidget,Ui_Settings):
+    def __init__(self,parent = None):
+        self.parent = parent
+        QWidget.__init__(self,parent)
+        self.setupUi(self)
+        #self.default_mode = MainWindow().get_darkmode()
+        #self.switch_modes(self.default_mode)
+
+        settings_buttons = [self.Set_default_user, self.initialize_devices, self.plot_appearance, self.save_location, self.connect_to_webserver]
+        for index, button in enumerate(settings_buttons):
+            button.clicked.connect(partial(self.stackedWidget_4.setCurrentIndex, index))
+        self.pushButton_13.clicked.connect(self.initialize_camera)
+        self.quit_settings.clicked.connect(self.quit)
+
+
+    def switch_modes(self, default_mode):
+        with open('Menu_button.css') as file:
+            self.setStyleSheet(self.start.styleSheet() +file.read().format(**os.environ))
+
+
+    def initialize_camera(self):
+        """ Basler camera initalizing  
+        ToDO: Error handling, add multiple camera possibilites for capturing in the dropdown menu"""
+
+        print("stuff worked")
+        self.camera = BayerCamera()
+        #initialize the camera 
+        camera_status = self.camera.init_camera()
+        self.scence_trial = QGraphicsScene(self) # generate a graphics scence in which the image can be putted
+        if camera_status is None: # initialization of the camera and error response if not correctly initialized
+            print("Camera is working")
+        else:
+            print("Camera is connected")
+
+
+    def quit(self):
+        self.close()
+    
+
+
+    
+
