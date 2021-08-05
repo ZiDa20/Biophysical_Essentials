31dd8ff9898593e3958789820f4f8f5460ba6a7c
David <david.zimmermann@tu-dresden.de>
2021-08-05 23:01:57 +0200

    introduced layouts in  main and offline analysis designer files (dav_update directory) and set to expanding - left side menu not working, but resizing and button clicks work

--- QT_GUI/offline_analysis_designer_object.py
+++ QT_GUI/offline_analysis_designer_object.py
@@ -18,161 +18,239 @@ class Ui_Offline_Analysis(object):
     def setupUi(self, Offline_Analysis):
         if not Offline_Analysis.objectName():
             Offline_Analysis.setObjectName(u"Offline_Analysis")
-        Offline_Analysis.resize(1663, 943)
+        Offline_Analysis.resize(1663, 962)
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Offline_Analysis.sizePolicy().hasHeightForWidth())
+        Offline_Analysis.setSizePolicy(sizePolicy)
+        self.gridLayout = QGridLayout(Offline_Analysis)
+        self.gridLayout.setObjectName(u"gridLayout")
         self.Offline_Analysis_Notebook = QTabWidget(Offline_Analysis)
         self.Offline_Analysis_Notebook.setObjectName(u"Offline_Analysis_Notebook")
-        self.Offline_Analysis_Notebook.setGeometry(QRect(0, 0, 1661, 951))
         self.Offline_Analysis_Notebook.setTabShape(QTabWidget.Rounded)
         self.Start_Analysis = QWidget()
         self.Start_Analysis.setObjectName(u"Start_Analysis")
+        sizePolicy.setHeightForWidth(self.Start_Analysis.sizePolicy().hasHeightForWidth())
+        self.Start_Analysis.setSizePolicy(sizePolicy)
+        self.gridLayout_3 = QGridLayout(self.Start_Analysis)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
         self.offline_analysis_widgets = QStackedWidget(self.Start_Analysis)
         self.offline_analysis_widgets.setObjectName(u"offline_analysis_widgets")
-        self.offline_analysis_widgets.setGeometry(QRect(0, 0, 1651, 851))
+        sizePolicy.setHeightForWidth(self.offline_analysis_widgets.sizePolicy().hasHeightForWidth())
+        self.offline_analysis_widgets.setSizePolicy(sizePolicy)
         self.start_page = QWidget()
         self.start_page.setObjectName(u"start_page")
-        self.new_analysis_with_template = QPushButton(self.start_page)
-        self.new_analysis_with_template.setObjectName(u"new_analysis_with_template")
-        self.new_analysis_with_template.setGeometry(QRect(550, 330, 181, 61))
-        self.view_analysis_from_db = QPushButton(self.start_page)
-        self.view_analysis_from_db.setObjectName(u"view_analysis_from_db")
-        self.view_analysis_from_db.setGeometry(QRect(550, 400, 181, 61))
+        sizePolicy.setHeightForWidth(self.start_page.sizePolicy().hasHeightForWidth())
+        self.start_page.setSizePolicy(sizePolicy)
+        self.gridLayout_4 = QGridLayout(self.start_page)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
         self.start_label = QLabel(self.start_page)
         self.start_label.setObjectName(u"start_label")
-        self.start_label.setGeometry(QRect(690, 170, 251, 41))
+        sizePolicy.setHeightForWidth(self.start_label.sizePolicy().hasHeightForWidth())
+        self.start_label.setSizePolicy(sizePolicy)
         font = QFont()
         font.setPointSize(20)
         self.start_label.setFont(font)
+
+        self.gridLayout_4.addWidget(self.start_label, 0, 0, 1, 1)
+
         self.blank_analysis_button = QPushButton(self.start_page)
         self.blank_analysis_button.setObjectName(u"blank_analysis_button")
-        self.blank_analysis_button.setGeometry(QRect(550, 260, 181, 61))
+        sizePolicy.setHeightForWidth(self.blank_analysis_button.sizePolicy().hasHeightForWidth())
+        self.blank_analysis_button.setSizePolicy(sizePolicy)
+
+        self.gridLayout_4.addWidget(self.blank_analysis_button, 1, 0, 1, 1)
+
         self.user_feedback_label = QLabel(self.start_page)
         self.user_feedback_label.setObjectName(u"user_feedback_label")
-        self.user_feedback_label.setGeometry(QRect(810, 260, 321, 201))
+        sizePolicy.setHeightForWidth(self.user_feedback_label.sizePolicy().hasHeightForWidth())
+        self.user_feedback_label.setSizePolicy(sizePolicy)
+
+        self.gridLayout_4.addWidget(self.user_feedback_label, 2, 0, 1, 1)
+
+        self.new_analysis_with_template = QPushButton(self.start_page)
+        self.new_analysis_with_template.setObjectName(u"new_analysis_with_template")
+        sizePolicy.setHeightForWidth(self.new_analysis_with_template.sizePolicy().hasHeightForWidth())
+        self.new_analysis_with_template.setSizePolicy(sizePolicy)
+
+        self.gridLayout_4.addWidget(self.new_analysis_with_template, 3, 0, 1, 1)
+
+        self.view_analysis_from_db = QPushButton(self.start_page)
+        self.view_analysis_from_db.setObjectName(u"view_analysis_from_db")
+        sizePolicy.setHeightForWidth(self.view_analysis_from_db.sizePolicy().hasHeightForWidth())
+        self.view_analysis_from_db.setSizePolicy(sizePolicy)
+
+        self.gridLayout_4.addWidget(self.view_analysis_from_db, 4, 0, 1, 1)
+
         self.offline_analysis_widgets.addWidget(self.start_page)
         self.blank_analysis = QWidget()
         self.blank_analysis.setObjectName(u"blank_analysis")
-        self.compare_series = QPushButton(self.blank_analysis)
-        self.compare_series.setObjectName(u"compare_series")
-        self.compare_series.setGeometry(QRect(430, 750, 771, 41))
+        self.gridLayout_5 = QGridLayout(self.blank_analysis)
+        self.gridLayout_5.setObjectName(u"gridLayout_5")
         self.widget_sepcific_label = QLabel(self.blank_analysis)
         self.widget_sepcific_label.setObjectName(u"widget_sepcific_label")
-        self.widget_sepcific_label.setGeometry(QRect(520, 10, 361, 41))
         self.widget_sepcific_label.setFont(font)
+
+        self.gridLayout_5.addWidget(self.widget_sepcific_label, 0, 3, 1, 1)
+
         self.select_directory_button = QPushButton(self.blank_analysis)
         self.select_directory_button.setObjectName(u"select_directory_button")
-        self.select_directory_button.setGeometry(QRect(90, 70, 121, 31))
+
+        self.gridLayout_5.addWidget(self.select_directory_button, 1, 0, 1, 1)
+
         self.selected_directory = QLabel(self.blank_analysis)
         self.selected_directory.setObjectName(u"selected_directory")
-        self.selected_directory.setGeometry(QRect(240, 70, 581, 31))
-        self.verticalLayoutWidget = QWidget(self.blank_analysis)
-        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(570, 250, 631, 441))
-        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+
+        self.gridLayout_5.addWidget(self.selected_directory, 1, 1, 1, 3)
+
+        self.filter_box = QGroupBox(self.blank_analysis)
+        self.filter_box.setObjectName(u"filter_box")
+        self.gridLayout_9 = QGridLayout(self.filter_box)
+        self.gridLayout_9.setObjectName(u"gridLayout_9")
+        self.tabWidget_2 = QTabWidget(self.filter_box)
+        self.tabWidget_2.setObjectName(u"tabWidget_2")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.gridLayout_10 = QGridLayout(self.tab)
+        self.gridLayout_10.setObjectName(u"gridLayout_10")
+        self.label = QLabel(self.tab)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout_10.addWidget(self.label, 0, 0, 1, 1)
+
+        self.pushButton = QPushButton(self.tab)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.gridLayout_10.addWidget(self.pushButton, 0, 1, 1, 1)
+
+        self.tabWidget_2.addTab(self.tab, "")
+
+        self.gridLayout_9.addWidget(self.tabWidget_2, 0, 0, 1, 1)
+
+        self.gridLayout_5.addWidget(self.filter_box, 2, 0, 1, 4)
+
         self.directory_tree_widget = QTabWidget(self.blank_analysis)
         self.directory_tree_widget.setObjectName(u"directory_tree_widget")
-        self.directory_tree_widget.setGeometry(QRect(90, 220, 301, 571))
         self.selected_tree_view = QWidget()
         self.selected_tree_view.setObjectName(u"selected_tree_view")
+        self.gridLayout_7 = QGridLayout(self.selected_tree_view)
+        self.gridLayout_7.setObjectName(u"gridLayout_7")
         self.experiments_tree_view = QTreeWidget(self.selected_tree_view)
         self.experiments_tree_view.setObjectName(u"experiments_tree_view")
-        self.experiments_tree_view.setGeometry(QRect(0, 0, 301, 551))
+
+        self.gridLayout_7.addWidget(self.experiments_tree_view, 0, 0, 1, 1)
+
         self.directory_tree_widget.addTab(self.selected_tree_view, "")
         self.discarded_tree_view = QWidget()
         self.discarded_tree_view.setObjectName(u"discarded_tree_view")
+        self.gridLayout_8 = QGridLayout(self.discarded_tree_view)
+        self.gridLayout_8.setObjectName(u"gridLayout_8")
         self.outfiltered_tree_view = QTreeWidget(self.discarded_tree_view)
         __qtreewidgetitem = QTreeWidgetItem()
         __qtreewidgetitem.setText(0, u"Objects");
         self.outfiltered_tree_view.setHeaderItem(__qtreewidgetitem)
         self.outfiltered_tree_view.setObjectName(u"outfiltered_tree_view")
-        self.outfiltered_tree_view.setGeometry(QRect(0, 0, 301, 551))
+
+        self.gridLayout_8.addWidget(self.outfiltered_tree_view, 0, 0, 1, 1)
+
         self.directory_tree_widget.addTab(self.discarded_tree_view, "")
+
+        self.gridLayout_5.addWidget(self.directory_tree_widget, 3, 0, 3, 2)
+
         self.pushButton_2 = QPushButton(self.blank_analysis)
         self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(420, 240, 75, 51))
+
+        self.gridLayout_5.addWidget(self.pushButton_2, 3, 2, 1, 1)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+
+        self.gridLayout_5.addLayout(self.verticalLayout, 3, 3, 2, 1)
+
         self.pushButton_3 = QPushButton(self.blank_analysis)
         self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setGeometry(QRect(420, 320, 75, 51))
-        self.gridLayoutWidget_2 = QWidget(self.blank_analysis)
-        self.gridLayoutWidget_2.setObjectName(u"gridLayoutWidget_2")
-        self.gridLayoutWidget_2.setGeometry(QRect(570, 810, 239, 41))
-        self.gridLayout_2 = QGridLayout(self.gridLayoutWidget_2)
+
+        self.gridLayout_5.addWidget(self.pushButton_3, 4, 2, 1, 1)
+
+        self.compare_series = QPushButton(self.blank_analysis)
+        self.compare_series.setObjectName(u"compare_series")
+
+        self.gridLayout_5.addWidget(self.compare_series, 5, 2, 1, 2)
+
+        self.gridLayout_2 = QGridLayout()
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.pushButton_4 = QPushButton(self.gridLayoutWidget_2)
+        self.pushButton_4 = QPushButton(self.blank_analysis)
         self.pushButton_4.setObjectName(u"pushButton_4")
 
         self.gridLayout_2.addWidget(self.pushButton_4, 0, 0, 1, 1)
 
-        self.pushButton_5 = QPushButton(self.gridLayoutWidget_2)
+        self.pushButton_5 = QPushButton(self.blank_analysis)
         self.pushButton_5.setObjectName(u"pushButton_5")
 
         self.gridLayout_2.addWidget(self.pushButton_5, 0, 1, 1, 1)
 
-        self.pushButton_6 = QPushButton(self.gridLayoutWidget_2)
+        self.pushButton_6 = QPushButton(self.blank_analysis)
         self.pushButton_6.setObjectName(u"pushButton_6")
 
         self.gridLayout_2.addWidget(self.pushButton_6, 0, 2, 1, 1)
 
-        self.filter_box = QGroupBox(self.blank_analysis)
-        self.filter_box.setObjectName(u"filter_box")
-        self.filter_box.setGeometry(QRect(90, 120, 1141, 51))
-        self.tabWidget_2 = QTabWidget(self.filter_box)
-        self.tabWidget_2.setObjectName(u"tabWidget_2")
-        self.tabWidget_2.setGeometry(QRect(30, 30, 731, 80))
-        self.tab = QWidget()
-        self.tab.setObjectName(u"tab")
-        self.label = QLabel(self.tab)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(50, 20, 111, 16))
-        self.pushButton = QPushButton(self.tab)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(270, 20, 171, 23))
-        self.tabWidget_2.addTab(self.tab, "")
+        self.gridLayout_5.addLayout(self.gridLayout_2, 6, 3, 1, 1)
+
         self.offline_analysis_widgets.addWidget(self.blank_analysis)
         self.analysis_specific_notebook = QWidget()
         self.analysis_specific_notebook.setObjectName(u"analysis_specific_notebook")
+        self.gridLayout_6 = QGridLayout(self.analysis_specific_notebook)
+        self.gridLayout_6.setObjectName(u"gridLayout_6")
         self.tabWidget = QTabWidget(self.analysis_specific_notebook)
         self.tabWidget.setObjectName(u"tabWidget")
-        self.tabWidget.setGeometry(QRect(0, 0, 1661, 931))
         self.template_tab = SpecificAnalysisTab()
         self.template_tab.setObjectName(u"template_tab")
         self.tabWidget.addTab(self.template_tab, "")
+
+        self.gridLayout_6.addWidget(self.tabWidget, 0, 0, 1, 1)
+
         self.offline_analysis_widgets.addWidget(self.analysis_specific_notebook)
+
+        self.gridLayout_3.addWidget(self.offline_analysis_widgets, 0, 0, 1, 1)
+
         self.Offline_Analysis_Notebook.addTab(self.Start_Analysis, "")
         self.visualization = QWidget()
         self.visualization.setObjectName(u"visualization")
         self.Offline_Analysis_Notebook.addTab(self.visualization, "")
 
+        self.gridLayout.addWidget(self.Offline_Analysis_Notebook, 0, 0, 1, 1)
+
         self.retranslateUi(Offline_Analysis)
 
         self.Offline_Analysis_Notebook.setCurrentIndex(0)
         self.offline_analysis_widgets.setCurrentIndex(0)
-        self.directory_tree_widget.setCurrentIndex(0)
         self.tabWidget_2.setCurrentIndex(0)
+        self.directory_tree_widget.setCurrentIndex(1)
 
         QMetaObject.connectSlotsByName(Offline_Analysis)
-
-    # setupUi
+        # setupUi
 
     def retranslateUi(self, Offline_Analysis):
         Offline_Analysis.setWindowTitle(QCoreApplication.translate("Offline_Analysis", u"Form", None))
+        self.start_label.setText(QCoreApplication.translate("Offline_Analysis", u"Select your Analysis", None))
+        self.blank_analysis_button.setText(QCoreApplication.translate("Offline_Analysis", u"New Blank Analysis", None))
+        self.user_feedback_label.setText(
+            QCoreApplication.translate("Offline_Analysis", u"Further Information about your selected analysis", None))
         self.new_analysis_with_template.setText(QCoreApplication.translate("Offline_Analysis", u"New Analysis using \n"
                                                                                                " previous analysis as template",
                                                                            None))
         self.view_analysis_from_db.setText(
             QCoreApplication.translate("Offline_Analysis", u"View Analysis From Database", None))
-        self.start_label.setText(QCoreApplication.translate("Offline_Analysis", u"Select your Analysis", None))
-        self.blank_analysis_button.setText(QCoreApplication.translate("Offline_Analysis", u"New Blank Analysis", None))
-        self.user_feedback_label.setText(
-            QCoreApplication.translate("Offline_Analysis", u"Further Information about your selected analysis", None))
-        self.compare_series.setText(
-            QCoreApplication.translate("Offline_Analysis", u"Select Series To Be Analyzed", None))
         self.widget_sepcific_label.setText(
             QCoreApplication.translate("Offline_Analysis", u"Configure your Analysis", None))
         self.select_directory_button.setText(QCoreApplication.translate("Offline_Analysis", u"Select Directory", None))
         self.selected_directory.setText(QCoreApplication.translate("Offline_Analysis", u"TextLabel", None))
+        self.filter_box.setTitle(QCoreApplication.translate("Offline_Analysis", u"Selected Filter", None))
+        self.label.setText(QCoreApplication.translate("Offline_Analysis", u"No filter selected", None))
+        self.pushButton.setText(QCoreApplication.translate("Offline_Analysis", u"Add Filter", None))
+        self.tabWidget_2.setTabText(self.tabWidget_2.indexOf(self.tab),
+                                    QCoreApplication.translate("Offline_Analysis", u"Add Filter", None))
         ___qtreewidgetitem = self.experiments_tree_view.headerItem()
         ___qtreewidgetitem.setText(2, QCoreApplication.translate("Offline_Analysis", u"Remove", None));
         ___qtreewidgetitem.setText(1, QCoreApplication.translate("Offline_Analysis", u"Show", None));
@@ -186,14 +264,11 @@ class Ui_Offline_Analysis(object):
                                               QCoreApplication.translate("Offline_Analysis", u"Discarded", None))
         self.pushButton_2.setText(QCoreApplication.translate("Offline_Analysis", u"Hold", None))
         self.pushButton_3.setText(QCoreApplication.translate("Offline_Analysis", u"Clear", None))
+        self.compare_series.setText(
+            QCoreApplication.translate("Offline_Analysis", u"Select Series To Be Analyzed", None))
         self.pushButton_4.setText(QCoreApplication.translate("Offline_Analysis", u"PushButton", None))
         self.pushButton_5.setText(QCoreApplication.translate("Offline_Analysis", u"PushButton", None))
         self.pushButton_6.setText(QCoreApplication.translate("Offline_Analysis", u"PushButton", None))
-        self.filter_box.setTitle(QCoreApplication.translate("Offline_Analysis", u"Selected Filter", None))
-        self.label.setText(QCoreApplication.translate("Offline_Analysis", u"No filter selected", None))
-        self.pushButton.setText(QCoreApplication.translate("Offline_Analysis", u"Add Filter", None))
-        self.tabWidget_2.setTabText(self.tabWidget_2.indexOf(self.tab),
-                                    QCoreApplication.translate("Offline_Analysis", u"Add Filter", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.template_tab),
                                   QCoreApplication.translate("Offline_Analysis", u"Tab 1", None))
         self.Offline_Analysis_Notebook.setTabText(self.Offline_Analysis_Notebook.indexOf(self.Start_Analysis),
