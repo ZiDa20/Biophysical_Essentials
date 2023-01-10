260ca4757721880332fc1d04eade09e42fe5e6db
mouzkolit <mouzkolit@outlook.de>
2023-01-10 13:03:35 +0100

    deleted old ui files

--- src/QT_GUI/ui_elements/OfflineAnalysis/ui_files/ui_offline_analysis_designer.py
+++ src/QT_GUI/ui_elements/OfflineAnalysis/ui_files/ui_offline_analysis_designer.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'dz_offline_analysis_mz_layouted.ui'
+## Form generated from reading UI file 'offline_analysis_designer.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.1.1
 ##
