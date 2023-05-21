e76ddd93b264fb27351f0ad95099525844d8963f
ZiDa20 <dave.england@gmx.de>
2023-05-21 10:39:14 +0200

    Rheoramp statistics implemented

--- src/QT_GUI/OfflineAnalysis/CustomWidget/statistics_function_table_handler.py
+++ src/QT_GUI/OfflineAnalysis/CustomWidget/statistics_function_table_handler.py
@@ -42,7 +42,7 @@ class StatisticsTablePromoted(QWidget, Ui_StatisticsTable):
         are_equal = df["experiment_name"] == df["meta_data"]
 
         if are_equal.all():
-            d = CustomErrorDialog("You have not set any meta data. Go back to the plots, select your meta data and come back to statistics", self.frontend_style)
+            d = CustomErrorDialog("Before running any statistics, setting meta data is required. Go back to the plots, select your meta data and come back to statistics", self.frontend_style)
             d.exec_()
         else:
             return
@@ -90,56 +90,54 @@ class StatisticsTablePromoted(QWidget, Ui_StatisticsTable):
             df = self.get_analysis_specific_statistics_df(i)
 
             unique_meta_data = list(df["meta_data"].unique())
-
-
-
-            # @todo whats this if needed for ??? dz 17.05.2023
-            if len(unique_meta_data) == len(df["meta_data"].values):
-                dialog = QDialog()
-
-                dialog.exec()
-
-            else:
                 
-                #for meta_data in unique_meta_data:
-                self.statistics_table_widget.setItem(row_to_insert,2, QTableWidgetItem('\n'.join(unique_meta_data)))
-                                                
-                #unique_meta_data.index(meta_data), 2,QTableWidgetItem(str(meta_data)))
+            #for meta_data in unique_meta_data:
+            self.statistics_table_widget.setItem(row_to_insert,2, QTableWidgetItem('\n'.join(unique_meta_data)))
+                                            
+            #unique_meta_data.index(meta_data), 2,QTableWidgetItem(str(meta_data)))
 
-                # show distrib�tion
-                self.data_dist  = QComboBox()
-                self.data_dist.addItems(["Normal Distribution", "Non-Normal Distribution"])
-                # "Bernoulli Distribution", "Binomial Distribution", "Poisson Distribution"
-                cell_widget = QWidget()
-                cell_widget_layout = QGridLayout()
-                cell_widget.setLayout(cell_widget_layout)
-                cell_widget_layout.addWidget(self.data_dist,0,0)
-                self.statistics_table_widget.setCellWidget(row_to_insert, 3, cell_widget)
+            # show distrib�tion
+            self.data_dist  = QComboBox()
+            self.data_dist.addItems(["Normal Distribution", "Non-Normal Distribution"])
+            # "Bernoulli Distribution", "Binomial Distribution", "Poisson Distribution"
 
-                # show test
-                self.stat_test = QComboBox()
-                self.stat_test.addItems(["Independent t-test","Welchs t-test", "Paired t-test", "Wilcoxon Signed-Rank test", "Kruskal Wallis test", "Brunner-Munzel test"])
-                self.statistics_table_widget.setCellWidget(row_to_insert, 4, self.stat_test)
+            cell_widget = QWidget()
+            cell_widget_layout = QGridLayout()
+            cell_widget.setLayout(cell_widget_layout)
+            cell_widget_layout.addWidget(self.data_dist,0,0)
+            self.statistics_table_widget.setCellWidget(row_to_insert, 3, cell_widget)
 
+            # show test
+            self.stat_test = QComboBox()
+            self.stat_test.addItems(["Independent t-test","Welchs t-test", "Paired t-test", "Wilcoxon Signed-Rank test", "Kruskal Wallis test", "Brunner-Munzel test"])
+            self.statistics_table_widget.setCellWidget(row_to_insert, 4, self.stat_test)
+
+            if "Result" in df.columns:
                 shapiro_test = stats.shapiro(df["Result"])
                 print(shapiro_test)
                 test_res = str(round(shapiro_test.pvalue,3))
-                if test_res == "0.0":
-                    cell_widget_layout.addWidget(QLabel("Shapiro Wilk Test \n p-Value < 0.001 "),1,0)
-                else:
-                    cell_widget_layout.addWidget(QLabel("Shapiro Wilk Test \n p-Value = " + shapiro_test),1,0)
-                if shapiro_test.pvalue >= 0.05:
-                    # evidence that data comes from normal distribution
-                    self.data_dist.setCurrentIndex(0)
-                    self.stat_test.setCurrentIndex(0)
-                else:
-                    # no evidence that data comes from normal distribution
-                    self.data_dist.setCurrentIndex(1)
-                    self.stat_test.setCurrentIndex(3)
+            elif "Rheoramp" in df.columns:
+                shapiro_test = stats.shapiro(df["Number AP"])
+                print(shapiro_test)
+                test_res = str(round(shapiro_test.pvalue,3))
+            
+            
+            if test_res == "0.0":
+                cell_widget_layout.addWidget(QLabel("Shapiro Wilk Test \n p-Value < 0.001 "),1,0)
+            else:
+                cell_widget_layout.addWidget(QLabel("Shapiro Wilk Test \n p-Value = " + test_res),1,0)
+            if shapiro_test.pvalue >= 0.05:
+                # evidence that data comes from normal distribution
+                self.data_dist.setCurrentIndex(0)
+                self.stat_test.setCurrentIndex(0)
+            else:
+                # no evidence that data comes from normal distribution
+                self.data_dist.setCurrentIndex(1)
+                self.stat_test.setCurrentIndex(3)
 
-                #self.statistics_add_meta_data_buttons[row_to_insert].clicked.connect(partial(self.select_statistics_meta_data, statistics_table_widget, row_to_insert))
-                
-                self.statistics_table_widget.show()
+            #self.statistics_add_meta_data_buttons[row_to_insert].clicked.connect(partial(self.select_statistics_meta_data, statistics_table_widget, row_to_insert))
+            
+            self.statistics_table_widget.show()
 
         start_statistics = QPushButton("Run Statistic Test")
         self.verticalLayout_2.addWidget(start_statistics)
@@ -170,80 +168,40 @@ class StatisticsTablePromoted(QWidget, Ui_StatisticsTable):
         for row in range(self.statistics_table_widget.rowCount()):
 
             df = self.get_analysis_specific_statistics_df(row)
-
-            # get the test to be performed from the combo box (position 4)
-            test_type = self.statistics_table_widget.cellWidget(row,4).currentText()
-
-            #meta_data = statistics_table.cellWidget(row,2).currentText()
-
-            # get unique meta data groups to compare
-            unique_groups  = list(df["meta_data"].unique())
-
-            # get a list of tuples for pairwise comparison
-            pairs = self.get_pairs(unique_groups)
+            test_type = self.statistics_table_widget.cellWidget(row,4).currentText()  # get the test to be performed from the combo box (position 4)
+            unique_groups  = list(df["meta_data"].unique()) # get unique meta data groups to compare
+            pairs = self.get_pairs(unique_groups) # get a list of tuples for pairwise comparison
             
-            # test if the table has column "voltage" - this means voltage steps and requieres to make pairs per voltage step (e.g. KO_80mV vs CTRL_80mV)
-            # test if the table has column "????"
+            # tables from voltage clamp analysis have a column "Voltage" to identify voltage step (e.g. KO_80mV vs CTRL_80mV)
+            # tables from rheoramp analysis will have a column "Rheoramp" to identify the rheoramp number
+            # 
 
             if "Voltage" in df.columns:
                 voltage_steps = list(df["Voltage"].unique())
-
-                # result data frame to be displayed
                 res_df = pd.DataFrame(columns=["Voltage", "Group_1", "Group_2", "p_Value"])
                 for v in voltage_steps:
                 
                     for p in pairs:
                         group1 = df[(df["meta_data"]==p[0]) & (df["Voltage"]==v)]["Result"]
                         group2 = df[(df["meta_data"]==p[1]) & (df["Voltage"]==v)]["Result"]
-
-                        if len(group2)>=len(group1):
-                            group2 = group2[0:len(group1)]
-                        else:
-                            group1 = group1[0:len(group2)]
-
-                        try:
-                            if test_type == "t-Test":
-                                res =  stats.ttest_ind(group1,group2)
-                            else:
-                                res = stats.wilcoxon(group1,group2)
-                            tmp = pd.DataFrame({"Voltage":v, "Group_1":[p[0]], "Group_2":[p[1]], "p_Value":[res.pvalue]})
-                        except Exception as e:
-                            #@todo better handling ? DZ
-                            print("Error in statistics", e)
-                            tmp = pd.DataFrame({"Voltage":v, "Group_1":[p[0]], "Group_2":[p[1]], "p_Value":"Error"})
-                       
-
+                        tmp = self.apply_stats_test(test_type,group1,group2, p, "Voltage", v)
                         res_df = pd.concat([res_df, tmp])
 
-                row_layout = QVBoxLayout()
-
-                label = QLabel("Statistics of " + self.statistics_table_widget.item(row,1).text())
-                #label.setAlignment(Qt.AlignCenter)
-                label.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
-                row_layout.addWidget(label)
-
-                label = QLabel("Performed Statistics:" + test_type)
-                #label.setAlignment(Qt.AlignCenter)
-                label.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
-                row_layout.addWidget(label)
-
-                statistics_table_view = QTableView()
-                #statistics_table_view.setAlignment(Qt.AlignCenter)
-                model = PandasTable(res_df)
-                statistics_table_view.setModel(model)
-                row_layout.addWidget(statistics_table_view)
+                self.create_statistics_for_steps(row, test_type,res_df)
                 
-                statistics_table_view.setSizeAdjustPolicy(QTableView.AdjustToContents)
 
-                # Set the size policy of the QTableView and its parent widget to MinimumExpanding
-                statistics_table_view.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
-                statistics_table_view.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+            elif "Rheoramp" in df.columns:
+                ramp_steps = list(df["Rheoramp"].unique())
+                res_df = pd.DataFrame(columns=["Rheoramp", "Group_1", "Group_2", "p_Value"])
+                for v in ramp_steps:
+                
+                    for p in pairs:
+                        group1 = df[(df["meta_data"]==p[0]) & (df["Rheoramp"]==v)]["Number AP"]
+                        group2 = df[(df["meta_data"]==p[1]) & (df["Rheoramp"]==v)]["Number AP"]
+                        tmp = self.apply_stats_test(test_type,group1,group2, p, "Rheoramp", v)
+                        res_df = pd.concat([res_df, tmp])
 
-                self.statistics_result_grid.addLayout(row_layout)
-                self.tabWidget.setCurrentIndex(1)
-
-            elif "Step" in df.columns:
-                print("not implemented yet")
+                self.create_statistics_for_steps(row, test_type,res_df)
 
             else: # simple boxplots for each meta data group to be compared
                 
@@ -251,18 +209,46 @@ class StatisticsTablePromoted(QWidget, Ui_StatisticsTable):
                 for p in pairs:
                     group1 = df[df["meta_data"]==p[0]]["Result"]
                     group2 = df[df["meta_data"]==p[1]]["Result"]
-                    if test_type == "t-Test":
-                        res =  stats.ttest_ind(group1,group2)
-                    else:
-                        print("not implemented yet")
-                    tmp = pd.DataFrame({"Group_1":[p[0]], "Group_2":[p[1]], "p_Value":[res.pvalue]})
+                    tmp = self.apply_stats_test(test_type,group1,group2, p)
                     res_df = pd.concat([res_df, tmp])
 
-                self.show_statistic_results(df, res_df, pairs)
+                self.show_statistic_results(df, res_df, pairs,test_type)
                 self.tabWidget.widget(1).show()
                 self.tabWidget.setCurrentIndex(1)
 
-    def show_statistic_results(self, df, statistics_df, pairs):
+    def create_statistics_for_steps(self, row:int, test_type:str, res_df:pd.DataFrame):
+        """
+        """
+        
+          
+        row_layout = QVBoxLayout()
+
+        label = QLabel("Statistics of " + self.statistics_table_widget.item(row,1).text())
+        #label.setAlignment(Qt.AlignCenter)
+        label.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        row_layout.addWidget(label)
+
+        label = QLabel("Performed Statistics:" + test_type)
+        #label.setAlignment(Qt.AlignCenter)
+        label.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        row_layout.addWidget(label)
+
+        statistics_table_view = QTableView()
+        #statistics_table_view.setAlignment(Qt.AlignCenter)
+        model = PandasTable(res_df)
+        statistics_table_view.setModel(model)
+        row_layout.addWidget(statistics_table_view)
+        
+        statistics_table_view.setSizeAdjustPolicy(QTableView.AdjustToContents)
+
+        # Set the size policy of the QTableView and its parent widget to MinimumExpanding
+        statistics_table_view.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        statistics_table_view.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+
+        self.statistics_result_grid.addLayout(row_layout)
+        self.tabWidget.setCurrentIndex(1)
+
+    def show_statistic_results(self, df, statistics_df, pairs,test_type):
         # plot the df as table
         import seaborn as sns
         import matplotlib.pyplot as plt
@@ -283,8 +269,14 @@ class StatisticsTablePromoted(QWidget, Ui_StatisticsTable):
         ax = fig.add_subplot(111)
         sns.boxplot(data=df,x ="meta_data" ,y = "Result", ax=ax)
         annotator = Annotator(ax, pairs, data=df, x ="meta_data" ,y = "Result")
+        test_identifier= {"Independent t-test": "t-test_ind",
+                          "Welchs t-test":"t-test_welch", 
+                          "Paired t-test":"test_paired",
+                           "Wilcoxon Signed-Rank test":"Wilcoxon", 
+                           "Kruskal Wallis test":"Kruskal"}
         # t-test_ind, t-test_welch, t-test_paired, Mann-Whitney, Mann-Whitney-gt, Mann-Whitney-ls, Levene, Wilcoxon, Kruskal, Brunner-Munzel.
-        annotator.configure(test='t-test_ind', text_format='star', loc='inside') #Mann-Whitney
+
+        annotator.configure(test=test_identifier[test_type], text_format='star', loc='inside') #Mann-Whitney
         annotator.apply_and_annotate()
         canvas = FigureCanvas(fig)
         self.statistics_result_grid.addWidget(canvas)
@@ -292,7 +284,36 @@ class StatisticsTablePromoted(QWidget, Ui_StatisticsTable):
         
         # make the seaborn plot
 
+    def apply_stats_test(self,test_type,group1,group2, group_pair, data_type=None, voltage=None):
+        """
+        apply a specific test to the specified data in group1 and group2
+        """
+        if len(group2)>=len(group1):
+            group2 = group2[0:len(group1)]
+        else:
+            group1 = group1[0:len(group2)]
+    
+        try:
+            if test_type == "Independent t-test":
+                res =  stats.ttest_ind(group1,group2)
+            elif test_type == "Wilcoxon Signed-Rank test":
+                res = stats.wilcoxon(group1,group2)
+            
+            if data_type: # for all voltage clamp recordings
+                tmp = pd.DataFrame({str(data_type):voltage, "Group_1":[group_pair[0]], "Group_2":[group_pair[1]], "p_Value":[res.pvalue]})
+            else:
+                tmp = pd.DataFrame({"Group_1":[group_pair[0]], "Group_2":[group_pair[1]], "p_Value":[res.pvalue]})
 
+        except Exception as e:
+            #@todo better handling ? DZ
+            print("Error in statistics", e)
+            if data_type == "Voltage":
+                tmp = pd.DataFrame({"Voltage":voltage, "Group_1":[group_pair[0]], "Group_2":[group_pair[1]], "p_Value":["Error"]})
+            else:
+                tmp = pd.DataFrame({"Group_1":[group_pair[0]], "Group_2":[group_pair[1]], "p_Value":["Error"]})
+
+        return tmp
+    
 
     def get_pairs(self, item_list):
         # Initialize an empty list to store the pairs
