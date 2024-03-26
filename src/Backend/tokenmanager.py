400da6a1981481d1c6cbb735ec34777bfbf31d3d
ZiDa20 <dave.england@gmx.de>
2024-03-26 19:39:44 +0100

    introduced a token manager

---
+++ src/Backend/tokenmanager.py
@@ -0,0 +1,6 @@
+class TokenManager:
+    # basic token manager implementation
+    BUNDLED_HEKA_DATA = "BUNDLED_HEKA_DATA"
+    UNBUNDLED_HEKA_DATA = "UNBUNDLED_HEKA_DATA"
+    ABF_DATA = "ABF_DATA"
+    NANION_DATA = "NANION_DATA"
