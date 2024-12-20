c947ce4aa835e9a23077b0223edcb10d50da2a26
ZiDa20 <dave.england@gmx.de>
2024-12-20 23:16:17 +0100

    important bugfix for correct calculation of area under the curve

--- src/Backend/OfflineAnalysis/AnalysisFunctions/AreaUnderTheCurve.py
+++ src/Backend/OfflineAnalysis/AnalysisFunctions/AreaUnderTheCurve.py
@@ -14,16 +14,18 @@ class AreaUnderTheCurve(SweepWiseAnalysisTemplate):
         self.function_name = 'AreaUnderTheCurve'
 
     def specific_calculation(self):
-        self.cslow_normalization = 1
-        return np.trapz(self.sliced_volt)
+        self.cslow_normalization = 0
+        y_signal_modified = self.sliced_volt -np.min(self.sliced_volt) # works for positive and negative curves
+        auc = np.trapz(y_signal_modified, self.sliced_time)
+        return auc
 
     def live_data_calculation(self):
         """
         the points that will be plotted during analysis function selection
         @return:
         """
-        self.cslow_normalization = 1
-        max_val = np.max(self.sliced_volt)
+        self.cslow_normalization = 0
+        max_val = np.min(self.sliced_volt)
         pos = np.where(self.sliced_volt == max_val)
         x_val = self.time[pos][0] + self.lower_bound
         return tuple((x_val, max_val))
